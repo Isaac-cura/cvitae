443a2e42550c4b940c05fb19507118ba21e98edc
José <demon.mystery@gmail.com>
2022-11-28 15:29:22 -0400

    feat: create profesional profile model

---
+++ src/domain/profesional-profile.model.ts
@@ -0,0 +1,41 @@
+export interface ProfesionalProfile {
+    name: string;
+    lastName: string;
+    avatarUrl: string;
+    description: string;
+    phone: string;
+    email: string;
+    socialNetwork: Array<{
+        icon?: string;
+        link: string;
+    }>
+    timeline: Array<Job>;
+    skills: Array<Skill>;
+    education: Array<Education>;
+    certifications: Array<Certification>;
+}
+
+export interface Skill {
+    iconUrl: string;
+    skill: string;
+    expertiseLevel: 1 | 2 | 3 | 4 | 5;
+}
+
+
+export interface Education {
+    title: string;
+    institution: string;
+    description?: string;
+    startDate?: string;
+    endDate?: string;
+}
+
+export interface Certification extends Education{}
+
+export interface Job {
+    company: string;
+    position: string;
+    description: string;
+    startDate: string;
+    endDate?: string;
+}
