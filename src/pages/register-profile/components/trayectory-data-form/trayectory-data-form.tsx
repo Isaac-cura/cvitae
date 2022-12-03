048399a6f74e41244c75906806df8c9c773d04b4
José <demon.mystery@gmail.com>
2022-12-03 07:03:17 -0400

    refactor: trajectory form component to remove useless code

--- src/pages/register-profile/components/trayectory-data-form/trayectory-data-form.tsx
+++ src/pages/register-profile/components/trayectory-data-form/trayectory-data-form.tsx
@@ -4,7 +4,7 @@ import { useForm } from "react-hook-form";
 import { DatePickerWrapped } from "../../../../components/wrapped-datepicker/wrapped-datepicker";
 export function TrayectoryDataForm() {
     const currentDate = useMemo(() => moment(), [])
-    const { control, register,watch, formState: {isValid, errors} } = useForm({
+    const { control, register, formState: {isValid} } = useForm({
         defaultValues: {
             startDate: currentDate as any,
             position: '',
@@ -14,65 +14,42 @@ export function TrayectoryDataForm() {
         },
         mode: "onChange",
     })
-    setTimeout(( ) => {
-        console.log(errors, isValid)
-
-    }, 1000)
     return <div className="row">
         <div className="col-12">
-            <h2>Trayectoria laboral {watch("company")} {watch("position")} {watch("description")}</h2>
+            <h2>Trayectoria laboral</h2>
         </div>
         <div className="col">
             <div className="row">
                 <div className="col" data-testid="startDate">
-                    <DatePickerWrapped
-                        control={control}
-                        label="fecha de contratación (*)"
-                        inputName="startDate"
-                    />
+                    <DatePickerWrapped control={control} label="fecha de contratación (*)" inputName="startDate"/>
                 </div>
             </div>
             <div className="row">
                 <div className="col">
                     <label htmlFor="jobPosition">posición (*)</label>
-                    <input
-                        className="form-control"
-                        type="text"
-                        id="jobPosition"
-                        {...register("position", {required: true})}
+                    <input className="form-control" type="text" id="jobPosition" {...register("position", {required: true})}
                     />
                 </div>
                 <div className="col">
                     <label htmlFor="jobCompany">compañía (*)</label>
-                    <input
-                        {...register("company", {required: true})}
-                        className="form-control"
-                        type="text"
-                        id="jobCompany" />
+                    <input {...register("company", {required: true})} className="form-control" type="text" id="jobCompany" />
                 </div>
             </div>
             <div className="row">
                 <div className="col">
                     <label htmlFor="jobDescription">descripción del puesto (*)</label>
-                    <textarea
-                        {...register("description", {required: true})}
-                        className="form-control"
-                        id="jobDescription">
+                    <textarea {...register("description", {required: true})} className="form-control" id="jobDescription">
                     </textarea>
                 </div>
             </div>
             <div className="row">
                 <div className="col">
-                    <DatePickerWrapped
-                        control={control}
-                        label="fecha de cese laboral"
-                        inputName="endDate"
+                    <DatePickerWrapped control={control} label="fecha de cese laboral" inputName="endDate"
                     />
                 </div>
             </div>
             <div className="row">
                 <div className="col">
-                    {isValid || JSON.stringify(errors)}
                     <button disabled={!isValid}>agregar</button>
                 </div>
             </div>
